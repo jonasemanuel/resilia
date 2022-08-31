f5d93310d21a150b0b69e9e2aa4714a4a76ee2ba
Jonas Cruz <jonas.cruz@g.globo>
2022-08-31 10:12:38 -0300

    Remove unused folders

--- modulo5/poke-agenda-1/src/components/Button.jsx
+++
@@ -1,3 +0,0 @@
-export const Button = (props) => {
-  return (<button onClick={props.callback}>{props.texto}</button>)
-}
--- modulo5/poke-agenda-1/src/service/requisicao.js
+++
@@ -1,8 +0,0 @@
-export function pokemon(nomePokemon) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/bulbasaur`).then((response) => response.json());
-}
-
-export async function pokemon2(nomePokemon) {
-  const requisicao = await fetch(`https://pokeapi.co/api/v2/pokemon/bulbasaur`)
-  return requisicao.json()
-}
